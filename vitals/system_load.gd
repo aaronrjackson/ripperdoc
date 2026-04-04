ea496bc5af31ed1b4b8accbf07f59860dd3d07cd
Aaron Jackson <aaronjackson@mines.edu>
2026-04-04 15:22:38 -0600

    dials now scale with size

--- vitals/system_load.gd
+++ vitals/system_load.gd
@@ -2,16 +2,24 @@ extends Panel
 
 @export var radius := 100
 @export var fill := 0.3 # 0.0 - 1.0
+@export var epsilon = 0.05 # overlap to close gap
 
 func _draw():
 	var center = size / 2
-	var sweep = fill * ((4 * PI) / 3)  # map 0.0-1.0 to full arc range
+	var r = min(size.x, size.y) * 0.5
+	var sweep = fill * ((4 * PI) / 3)
+	var arc_width = r * 1.0  # scales with radius
 	
-	draw_circle(center, radius, Color("fd0156ff"))
-	draw_circle(center, radius - 8, Color("121016ff"))
-	draw_arc(center, 42, ((5 * PI) / 6), ((5 * PI) / 6) + sweep, 100, Color("ed4800ff"), 100)  # fill
-	draw_arc(center, 50, ((5 * PI) / 6), (PI / 6), 100, Color("fd0156ff"), 100)  # bottom
-	draw_circle(center, 36, Color("fd0156ff"))
+	# outline
+	draw_circle(center, r, Color("fd0156ff"))
+	# inside background
+	draw_circle(center, r - (r * 0.04), Color("121016ff"))
+	# fill meter
+	draw_arc(center, r * 0.46, ((5 * PI) / 6) - epsilon, ((5 * PI) / 6) + sweep + epsilon, 100, Color("ed4800ff"), arc_width)
+	# bottom thick
+	draw_arc(center, r * 0.50, ((5 * PI) / 6), (PI / 6), 100, Color("fd0156ff"), arc_width)
+	# middle inner tiny circle
+	draw_circle(center, r * 0.36, Color("fd0156ff"))
 
 func _ready() -> void:
 	queue_redraw()
@@ -19,3 +27,7 @@ func _ready() -> void:
 func set_fill(value: float) -> void:
 	fill = clamp(value, 0.0, 1.0)
 	queue_redraw()
+
+func _notification(what: int) -> void:
+	if what == NOTIFICATION_RESIZED:
+		queue_redraw()
--- vitals/vitals.gd
+++ vitals/vitals.gd
@@ -1,7 +1,7 @@
 extends Control
 
-@onready var load_dial = $VBoxContainer/HBoxContainer/LoadDial
-@onready var pressure_dial = $VBoxContainer/HBoxContainer/PressureDial
+@onready var load_dial = $VBoxContainer/DialsBackground/MarginContainer/HBoxContainer/LoadDial
+@onready var pressure_dial = $VBoxContainer/DialsBackground/MarginContainer/HBoxContainer/PressureDial
 
 func _ready() -> void:
 	GameManager.vitals_changed.connect(_on_vitals_changed)
