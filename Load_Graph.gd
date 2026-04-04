3910df5e89d820d4b40ed8f35cd80c971891812b
Me_ <bkimbelljack@gmail.com>
2026-04-04 11:23:03 -0600

    ui color scheme changes

--- Load_Graph.gd
+++ Load_Graph.gd
@@ -8,11 +8,11 @@ func _draw():
 	var center = size / 2
 	
 	# Draw the circle
-	draw_circle(center, radius, Color.BLACK)
-	draw_circle(center, radius - 8, Color.GRAY)
+	draw_circle(center, radius, Color("fd0156ff"))
+	draw_circle(center, radius - 8, Color("1C1829"))
 	draw_arc(center, 42, (((5* PI) / 6) - .1), (((5* PI) / 6) + fill), 100, Color.DARK_ORANGE, 100)
-	draw_circle(center, 40, Color.BLACK)
-	draw_arc(center, 50, ((5* PI) / 6), ((PI) / 6), 100, Color.BLACK, 100)
+	draw_circle(center, 40, Color("fd0156ff"))
+	draw_arc(center, 50, ((5* PI) / 6), ((PI) / 6), 100, Color("fd0156ff"), 100)
 
 
 func _ready() -> void:
