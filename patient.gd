8371488e0638c1d7f8e54719da04b75a5b9669c2
Aaron Jackson <aaronjackson@mines.edu>
2026-04-04 22:04:46 -0600

    improved death sound

--- patient.gd
+++ patient.gd
@@ -2,7 +2,9 @@ extends Control
 
 @onready var base = $Layers/Base
 @onready var death_sound = $DeathPlayer
+@onready var flatline_sound = $FlatlinePlayer
 
+var flatline_tween: Tween = null
 var death_tween: Tween = null
 
 func _ready() -> void:
@@ -10,7 +12,16 @@ func _ready() -> void:
 	GameManager.character_loaded.connect(_on_character_loaded)
 
 func _on_character_died() -> void:
-	_play_death_sound()
+	death_sound.play()
+	flatline_sound.play()
+	flatline_sound.volume_db = -32.0
+	# start fade after a delay
+	flatline_tween = create_tween()
+	flatline_tween.tween_interval(4.0)  # play at full volume for 5 seconds
+	flatline_tween.tween_method(
+		func(v): flatline_sound.volume_db = v, -32.0, -80.0, 4.0)  # fade over 4 seconds
+	flatline_tween.tween_callback(func(): flatline_sound.stop())
+	
 	var mat = base.material as ShaderMaterial
 	if mat == null:
 		return
@@ -21,6 +32,7 @@ func _on_character_died() -> void:
 	death_tween.tween_property(base, "modulate:a", 0.0, 6)
 
 func _on_character_loaded(character: Character) -> void:
+	flatline_sound.stop()
 	# kill any running death tween before resetting
 	if death_tween != null and death_tween.is_running():
 		death_tween.kill()
@@ -29,10 +41,3 @@ func _on_character_loaded(character: Character) -> void:
 		return
 	mat.set_shader_parameter("glitch_intensity", 0.0)
 	base.modulate.a = 1.0
-
-func _play_death_sound() -> void:
-	var sound = death_sound
-	if sound == null:
-		print("ERROR: DeathPlayer not found in Patient!!!")
-		return
-	sound.play()
